a770738698f0d584e5498f0a7d2aee9aaac9e714
KazekinRoman <romanich569@gmail.com>
2023-10-31 00:22:38 +0200

    HTML Update

--- src/App.tsx
+++ src/App.tsx
@@ -78,59 +78,70 @@ function App() {
 
     const saveResultsAsHTML = async (userName: string) => {
         const styles = `
-      @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400&display=swap');
-      body {
-        font-family: 'Arial', sans-serif;
-        text-align: center;
-        padding: 20px;
-      }
-      h1 {
-        font-size: 2em;
-        margin-bottom: 20px;
-      }
-      ul {
-        list-style: none;
-        padding: 0;
-        max-width: 600px;
-        width: 100%;
-        margin: 0 auto;
-      }
-      li {
-        margin-bottom: 10px;
-      }
-      strong {
-        color: #4CAF50;
-      }
-      p {
-        font-family: 'Roboto Mono', monospace;
-        margin: 10px 0;
-      }
-    `;
+  @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400&display=swap');
+  body {
+    font-family: 'Roboto Mono', monospace;
+    padding: 20px;
+    margin: 0;
+    color: black;
+    text-align: center;
+  }
+   .question-block {
+    text-align: left;
+  }
+  h1 {
+    font-size: 2em;
+    margin-bottom: 20px;
+  }
+  .info, .info div {
+    margin-bottom: 20px;
+  }
+  .info strong, li strong {
+    color: #4CAF50;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    max-width: 600px;
+    width: 100%;
+    margin: 0 auto;
+  }
+  li {
+    margin-bottom: 10px;
+  }
+  p {
+    font-family: 'Roboto Mono', monospace;
+    margin: 0;
+  }
+`;
 
         const htmlContent = `
-      <html lang = "ru">
-      <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Результаты Теста</title>
-        <style>${styles}</style>
-      </head>
-      <body>
-        <h1>Результаты Теста</h1>
-        <p><strong>Имя:</strong> ${userName}</p>
-        <p><strong>Оставшееся время:</strong> ${formatTime()}</p>
-        <ul>
-          ${questions.map(q => `
-            <li>
-              <strong>${q.game}:</strong> ${q.question}
-              <p>Ответ: ${q.answer}</p>
-            </li>
-          `).join('')}
-        </ul>
-      </body>
-      </html>
-    `;
+  <html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Результаты Теста</title>
+    <style>${styles}</style>
+  </head>
+  <body>
+    <h1>Результаты Теста</h1>
+    <div class="info">
+      <div><strong>Имя:</strong> ${userName}</div>
+      <div><strong>Оставшееся время:</strong> ${formatTime()}</div>
+    </div>
+    <ul>
+      ${questions.map(q => `
+        <li class="question-block">
+          <strong>${q.game}:</strong> ${q.question}
+          <p>Ответ: ${q.answer}</p>
+        </li>
+      `).join('')}
+    </ul>
+  </body>
+  </html>
+`;
+
 
         const blob = new Blob([htmlContent], { type: 'text/html' });
         const formData = new FormData();
